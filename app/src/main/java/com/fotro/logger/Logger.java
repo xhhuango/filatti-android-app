223ad7ecee573cd6dee6346a65d7b083ac1bd8e0
Xuan Hau Huang <xhhuango@gmail.com>
2016-06-02 21:37:18 +0200

    Add logger

---
+++ app/src/main/java/com/fotro/logger/Logger.java
@@ -0,0 +1,24 @@
+package com.fotro.logger;
+
+import android.util.Log;
+
+public final class Logger {
+    private Logger() {
+    }
+
+    public static void debug(String tag, String msg) {
+        Log.d(tag, msg);
+    }
+
+    public static void debug(String tag, String msg, Throwable throwable) {
+        Log.d(tag, msg, throwable);
+    }
+
+    public static void error(String tag, String msg) {
+        Log.e(tag, msg);
+    }
+
+    public static void error(String tag, String msg, Throwable throwable) {
+        Log.e(tag, msg, throwable);
+    }
+}
