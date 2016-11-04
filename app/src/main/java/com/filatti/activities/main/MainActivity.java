1ecb44eaf0394c9d3cfb9ed86df6263cc1d905cc
Xuan Hau Huang <xhhuango@gmail.com>
2016-11-04 22:34:50 +0100

    #27 Apply Tab layout in MainActivity

--- app/src/main/java/com/filatti/activities/main/MainActivity.java
+++ app/src/main/java/com/filatti/activities/main/MainActivity.java
@@ -2,8 +2,7 @@ package com.filatti.activities.main;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.TabLayout;
 
 import com.filatti.R;
 import com.filatti.activities.FilattiActivity;
@@ -15,16 +14,37 @@ public class MainActivity extends FilattiActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        initAlbumButton();
+        initTabLayou();
     }
 
-    private void initAlbumButton() {
-        Button albumButton = (Button) findViewById(R.id.albumButton);
-        albumButton.setOnClickListener(new View.OnClickListener() {
+    private void initTabLayou() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
-                startActivity(intent);
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+                    case 0:
+                        break;
+
+                    case 1:
+                        Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 2:
+                        break;
+
+                    default:
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                onTabSelected(tab);
             }
         });
     }
